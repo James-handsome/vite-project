7632d71aabe2727a0448539a126dfe840de76cec
zhanhao <zhanhao@ieggtc.com>
2025-04-30 16:06:39 +0800

    dev

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,17 +1,13 @@
 import React from "react";
-import { useRouteError } from "react-router-dom";
 import PropTypes from "prop-types";
 import "./ErrorPage.module.scss";
 
 const ErrorPage = ({
   defaultMessage = "Sorry, an unexpected error has occurred.",
 }) => {
-  const error = useRouteError();
-
   return (
     <div className="error-page">
       <div className="error-container">
-        <h1>{error}</h1>
         <h2>{defaultMessage}</h2>
       </div>
     </div>
